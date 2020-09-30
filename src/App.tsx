8b09f5ca6fc6ad71907d41c4cb2391cb31062d83
Daniele Santos <js_dani@hotmail.com>
2020-09-30 10:27:24 -0300

    Create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div />} />
+        <Route path="/create" element={<div />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
