395049ea66a7a64f1c24c9222a9a35ff0eb71c66
Daniele Santos <js_dani@hotmail.com>
2020-10-05 11:31:09 -0300

    Create responsive layout for header component

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -7,8 +7,12 @@ export const Container = styled.div`
   background-image: linear-gradient(230deg, #532d8c 0%, #f2295b 100%);
   align-items: center;
   justify-content: space-between;
-  box-sizing: border-box;
   box-shadow: 0 2px 10px -2px #888888;
+
+  @media (max-width: 1280px) {
+    display: flex;
+    width: 100%;
+  }
 `;
 
 export const LogoWrapper = styled.div`
@@ -19,12 +23,20 @@ export const LogoWrapper = styled.div`
     width: 40px;
     height: 40px;
     margin-left: 40px;
+
+    @media (max-width: 1280px) {
+      margin-left: 20px;
+    }
   }
 
   > span {
     color: #fff;
     font-weight: bold;
     margin-left: 15px;
+
+    @media (max-width: 400px) {
+      font-size: 15px;
+    }
   }
 `;
 
@@ -36,6 +48,10 @@ export const UserWrapper = styled.div`
     color: #fff;
     font-size: 13px;
     margin-right: 5px;
+
+    @media (max-width: 400px) {
+      display: none;
+    }
   }
 `;
 
@@ -48,6 +64,10 @@ export const Avatar = styled.div`
   border-radius: 50%;
   background-color: #f6f6f6;
   margin-right: 40px;
+
+  @media (max-width: 1280px) {
+    margin-right: 20px;
+  }
 
   > span {
     color: #2b2b2b;
